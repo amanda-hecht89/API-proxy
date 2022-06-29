3654ae6de93c1f2105eb25a8d348871dc86a27f0
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-06-29 17:53:58 -0600

    fetch yelp

--- src/services/FetchUtils.js
+++ src/services/FetchUtils.js
@@ -4,3 +4,10 @@ export async function getPokemon(searchFilter) {
 
   return data;
 }
+
+export async function getYelp(searchFilter) {
+  const rawResponse = await fetch(`https://api.yelp.com/v3/businesses/${searchFilter}`);
+  const data = await rawResponse.json();
+  
+  return data;
+}
